0f2e914bad57d8dfa6db54d59e2e9124df701a93
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-03-07 21:45:10 +0200

    Init commit for connect game

---
+++ learn-react-ts/connect-four-game/my-app/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import { render } from '@testing-library/react';
+
+let style = {
+	backgroundColor: 'white'
+}
+
+
+export default class App extends React.Component{
+
+	
+	
+
+	render() {
+		return (
+			<div>
+				
+			</div>
+		)
+	}
+}
