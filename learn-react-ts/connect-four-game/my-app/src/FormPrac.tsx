3be4f98aa1a7611c84ea7d16185b9f398da413c3
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-03-24 18:02:08 +0200

    things are things learning about forms in react

--- learn-react-ts/connect-four-game/my-app/src/FormPrac.tsx
+++ learn-react-ts/connect-four-game/my-app/src/FormPrac.tsx
@@ -3,28 +3,49 @@ import { isTSParenthesizedType } from '@babel/types';
 
 interface FormPracStates {
   userIn: string;
+  isChecked: boolean;
+  userText: string;
 }
 
 export default class FormPrac extends React.Component<{}, FormPracStates>{
   constructor(props: any) {
     super(props);
     this.state = {
-      userIn: ""
+      userIn: "",
+      isChecked: false,
+      userText: ""
     }
     this.handleInput = this.handleInput.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+    this.handleText = this.handleText.bind(this);
   }
 
   handleInput(e: any) {
     this.setState({userIn: e.target.value});
   }
 
+  handleCheck(e: any) {
+    this.setState({isChecked: e.target.checked});
+  }
+
+  handleText(e: any) {
+    this.setState({userText: e.target.value});
+  }
+
   render() {
     let userIn = this.state.userIn;
-    const handleInput = this.handleInput;
+    let checked: string = "";
+    let text = this.state.userText;
+    let isChecked = this.state.isChecked;
+    isChecked ? checked = "Checked" : checked = "Unchecked";
     return(
       <>
-        <input type="text" value={userIn} onChange={(e) => handleInput(e)}/>
+        <input type="text" value={userIn} onChange={(e) => this.handleInput(e)}/>
         <p>{userIn}</p>
+        <input type="checkbox" checked={isChecked} onChange={(e) => this.handleCheck(e)}/>
+        <p>{checked}</p>
+        <textarea cols={60} rows={7} onChange={(e) => this.handleText(e)}></textarea>
+        <p>The stuff you typed: {text}</p>
       </>
     )
   }
