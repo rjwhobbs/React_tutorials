91392739ab7306665576fb8b9e3e8fa67ef902d9
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-03-29 17:25:10 +0200

    Solution

---
+++ learn-react-ts/connect-four-game/my-app/src/DynamicArraySolution.tsx
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+
+function PostButton(props: any) {
+  let style = {
+    width: 24,
+    height: 24
+  }
+
+  return (
+    <button style={style} onClick={() => props.handleClick()}>{props.label}</button>
+  )
+}
+
+function PostText(props: any) {
+  let style = {
+    border: "1px solid red",
+    width: props.width
+  }
+
+  return (
+    <div style={style}>{props.text}</div>
+  )
+}
+
+function Post(props: any) {
+  let style = {
+    display: "flex"
+  }
+
+  return (
+    <div style={style}>
+      <PostButton label="X" handleClick={props.removeItem}/>
+      <PostText text={props.title} width="200" />
+      <PostButton label="+" handleClick={props.incScore}/>
+      <PostText text={props.score} width="20" />
+      <PostButton label="-" handleClick={props.decScore}/>
+    </div>
+  )
+}
+
+function PostList(props: any) {
+  let list = props.postList.map((item: any, index: any) => {
+    return (
+      <Post 
+        key={index}
+        title={item.title}
+        score={item.score} 
+        incScore={() => props.updateScore(index, 1)}
+        decScore={() => props.updateScore(index, -1)}
+        removeItem={() => props.removeItem(index)}/>
+    )
+  });
+  return (
+    <div>
+      {list}
+    </div>
+  )
+}
+
+interface DynArrStates {
+  value: string;
+  items: any;
+}
+
+class DynamicArraySolution extends Component<{}, DynArrStates>{
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      value: "",
+      items: []
+    }
+  }
+
+  handleChange = (e: any) => {
+    this.setState({value: e.target.value});
+  }
+
+  addItem = () => {
+    let itemsTemp = this.state.items.slice();
+    let userInput = this.state.value.substring(0, 20);
+    itemsTemp.push({title: userInput, score: 0});
+    itemsTemp.sort((a: any, b: any) => {
+      return b.score - a.score;
+    });
+    this.setState({items: itemsTemp, value: ""});
+  }
+
+  updateScore = (index: number, val: number) => {
+    let itemsTemp = this.state.items.slice();
+    itemsTemp[index].score += val;
+    itemsTemp.sort((a: any, b: any) => {
+      return b.score - a.score;
+    });
+    this.setState({items: itemsTemp});
+  }
+
+  removeItem = (index: number) => {
+    let itemsTemp = this.state.items.slice();
+    itemsTemp.splice(index, 1);
+    itemsTemp.sort((a: any, b: any) => {
+      return b.score - a.score;
+    });
+    this.setState({items: itemsTemp});
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        <button onClick={this.addItem}>Submit</button>
+        <PostList 
+          postList={this.state.items}
+          updateScore={this.updateScore} 
+          removeItem={this.removeItem}/>
+      </div>
+    );
+  }
+}
+
+export default DynamicArraySolution;
--- learn-react-ts/connect-four-game/my-app/src/index.tsx
+++ learn-react-ts/connect-four-game/my-app/src/index.tsx
@@ -6,11 +6,12 @@ import * as serviceWorker from './serviceWorker';
 import ArrayPrac from './ArrayPrac';
 import FormPrac from './FormPrac';
 import DynamicArrayPrac from './DynamicArrayPrac';
+import DynamicArraySolution from './DynamicArraySolution';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<ArrayPrac />, document.getElementById('root'));
 // ReactDOM.render(<FormPrac />, document.getElementById('root'));
-ReactDOM.render(<DynamicArrayPrac />, document.getElementById('root'));
+ReactDOM.render(<DynamicArraySolution />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
