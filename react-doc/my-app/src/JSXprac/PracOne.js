7cd1b899e46261cc38be7152e1493ee15ccf72e3
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-07-02 13:22:38 +0200

    Passing a function as a child propertry and then calling it in the component

--- react-doc/my-app/src/JSXprac/PracOne.js
+++ react-doc/my-app/src/JSXprac/PracOne.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 
-// function Array () {
-	
-// }
+function Repeat(props) {
+  let items = [];
+  for (let i = 0; i < props.numTimes; i++) {    
+		items.push(props.children(i));
+  }
+  return <div>{items}</div>;
+}
+
+function ListOfTenThings() {
+  return (
+    <Repeat numTimes={10}>
+      {(index) => <div key={index}>This is item {index} in the list</div>}    
+		</Repeat>
+  );
+}
 
 class PracOne extends Component {
 	render() {
@@ -11,6 +23,7 @@ class PracOne extends Component {
 			<li key="A">First item</li>,
 			<li key="B">Second item</li>,
 			<li key="C">Third item</li>,
+			<ListOfTenThings />
 		];
 	}
 }
