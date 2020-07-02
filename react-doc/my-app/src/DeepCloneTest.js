8feaa3a1c35451298791c7cca0360f576f0fab54
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-07-02 15:22:33 +0200

    How to properly do a deep copy

---
+++ react-doc/my-app/src/DeepCloneTest.js
@@ -0,0 +1,14 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+let obj1 = {a: 0, b: {c: 42}};
+let obj2 = Object.assign({}, obj1);
+// Better
+let obj3 = JSON.parse(JSON.stringify(obj1));
+
+obj1.a = 12;
+obj1.b.c = 99;
+
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
+
+
